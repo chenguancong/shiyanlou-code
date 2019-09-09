70dc6cbe14a40ded9f233826724ac15743ca8a72
chenguancong <1120083983@qq.com>
2019-09-09 17:44:53 +0800

    append python file

---
+++ jump7.py
@@ -0,0 +1,13 @@
+import time
+
+#num = int(input("???????"))
+
+for i in range(1,101):
+        if i%7 == 0:
+                    continue
+                    tem = i
+                        y = str(tem).find('7')
+                            if y != -1:
+                                        continue
+                                        print(i)
+
